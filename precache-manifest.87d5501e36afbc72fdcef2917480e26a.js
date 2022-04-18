2d509deec060c014e905adb6d7bf227a150e70b7
“frankqiang <tyq155888@163.com>
2022-04-18 22:49:22 +0800

    deploy

---
+++ precache-manifest.87d5501e36afbc72fdcef2917480e26a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ebc3fd88a667117ec59",
+    "url": "/booking-website/css/app.bb44cc7e.css"
+  },
+  {
+    "revision": "ed2b52cda380655725e6a6e719848400",
+    "url": "/booking-website/index.html"
+  },
+  {
+    "revision": "7ebc3fd88a667117ec59",
+    "url": "/booking-website/js/app.0b5fd79b.js"
+  },
+  {
+    "revision": "aac6d7697745ca937575",
+    "url": "/booking-website/js/chunk-vendors.1e71ba85.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/booking-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/booking-website/robots.txt"
+  }
+]);
